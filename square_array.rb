04f871bb63effa882f838043bdac705a8a69f12d
danny galimi <dannygalimi13@aim.com>
2020-08-19 23:54:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,16 @@
 def square_array(array)
+<<<<<<< HEAD
  arr = []
   array.each {|square| arr << square ** 2}
   arr
   end
   
+=======
+  new_array = []
+  array.each do |square| 
+  square ** 2
+  new_array <<
+  end
+  end
+>>>>>>> 7fccb1b98209a898d117fe654e2640fe8c5fffb0
   
